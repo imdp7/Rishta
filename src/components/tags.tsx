203005a7ada348b5ce0ddeff791589837e103b19
Darshan Patel <imdp7@Darshans-MacBook-Air.local>
2024-02-23 23:11:58 -0500

    updated minor changes v2

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -8,8 +8,8 @@ export default function Tags(props: Tag) {
   return (
     <div className="max-w-8xl mx-auto py-2">
       <div className="w-full border border-gray-500">
-        <h2 className="border-b border-gray-400 bg-gray-600 px-5 py-2 text-2xl font-semibold opacity-80">
-          {props?.title}
+        <h2 className="border-b border-gray-400 bg-gray-600 px-5 py-2 text-2xl font-semibold text-white opacity-80 shadow-2xl">
+          {props.title}
         </h2>
         <div className="flex flex-row flex-wrap justify-center space-x-4 py-2">
           {props.data?.map((tag: any, i: number) => (
